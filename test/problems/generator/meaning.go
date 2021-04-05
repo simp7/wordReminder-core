a92cf5830a2ed6441af32cd42134b85d7cbf392c
simp7 <JHParkOfficial@protonmail.com>
2021-04-05 09:25:56 +0900

    Inject dependency in generator by method.

--- test/problems/generator/meaning.go
+++ test/problems/generator/meaning.go
@@ -8,29 +8,26 @@ import (
 )
 
 type meaningGen struct {
-	words voca.WordSet
 }
 
-func Meaning(set voca.WordSet) *meaningGen {
-	m := new(meaningGen)
-	m.words = set
-	return m
+func Meaning() *meaningGen {
+	return new(meaningGen)
 }
 
 func (m *meaningGen) doEach(w voca.Word) test.Problem {
 	return problem.Meaning(w)
 }
 
-func (m *meaningGen) Do() test.Problems {
+func (m *meaningGen) Generate(words voca.WordSet) test.Problems {
 
-	if m.words.Len() == 0 {
+	if words.Len() == 0 {
 		return nil
 	}
 
 	result := problems.Array()
 
-	for i := 0; i < m.words.Len(); i += 1 {
-		result.Add(m.doEach(m.words.Get(i)))
+	for i := 0; i < words.Len(); i += 1 {
+		result.Add(m.doEach(words.Get(i)))
 	}
 
 	return result
